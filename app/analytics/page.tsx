088d9e92fcbf2ad4bd3ad99eb160d18e04fc91c0
owolfdev <owolfdev@gmail.com>
2023-07-10 04:34:54 +0700

    update analytics

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -1,4 +1,15 @@
-import { ToastDemo } from "@/components/custom-toast"
+import * as React from "react"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { ReChartBar } from "@/components/rechart-bar-chart"
+import { ReChartPie } from "@/components/rechart-pie-chart"
 
 export default function StatsPage() {
   return (
@@ -7,6 +18,32 @@ export default function StatsPage() {
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Analytics
         </h1>
+        <div className="flex flex-col w-full gap-8 h-[500px] mt-8">
+          <Card>
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              {/* <CardDescription>Card Description</CardDescription> */}
+            </CardHeader>
+            <CardContent>
+              <div>
+                <ReChartBar />
+              </div>
+            </CardContent>
+            {/* <CardFooter>
+            <p>Card Footer</p>
+          </CardFooter> */}
+          </Card>
+          {/* <Card className="w-full">
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div>
+                <ReChartPie />
+              </div>
+            </CardContent>
+          </Card> */}
+        </div>
       </div>
     </section>
   )
---
+++ components/rechart-pie-chart.tsx
@@ -0,0 +1,130 @@
+"use client"
+
+import {
+  Bar,
+  BarChart,
+  Cell,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from "recharts"
+
+interface DataItem {
+  name: string
+  total: number
+  color: string
+}
+
+const data: DataItem[] = [
+  {
+    name: "food",
+    total: Math.floor(Math.random() * 5000) + 1000,
+    color: "#adfa1d",
+  },
+  {
+    name: "clothing",
+    total: Math.floor(Math.random() * 5000) + 1000,
+    color: "#ff7f50",
+  },
+  {
+    name: "utilities",
+    total: Math.floor(Math.random() * 5000) + 1000,
+    color: "#3366cc",
+  },
+  {
+    name: "rent",
+    total: Math.floor(Math.random() * 5000) + 1000,
+    color: "#dc3912",
+  },
+  {
+    name: "transportation",
+    total: Math.floor(Math.random() * 5000) + 1000,
+    color: "#109618",
+  },
+  {
+    name: "entertainment",
+    total: Math.floor(Math.random() * 5000) + 1000,
+    color: "#990099",
+  },
+  {
+    name: "misc",
+    total: Math.floor(Math.random() * 5000) + 1000,
+    color: "#ff9900",
+  },
+  {
+    name: "school",
+    total: Math.floor(Math.random() * 5000) + 1000,
+    color: "#0099c6",
+  },
+  {
+    name: "pets",
+    total: Math.floor(Math.random() * 5000) + 1000,
+    color: "#dd4477",
+  },
+  {
+    name: "healty",
+    total: Math.floor(Math.random() * 5000) + 1000,
+    color: "#66aa00",
+  },
+  {
+    name: "grooming",
+    total: Math.floor(Math.random() * 5000) + 1000,
+    color: "#b82e2e",
+  },
+  {
+    name: "donations",
+    total: Math.floor(Math.random() * 5000) + 1000,
+    color: "#316395",
+  },
+]
+
+export function ReChartPie() {
+  return (
+    <>
+      {/* <ResponsiveContainer width="100%" height={350}>
+        <BarChart data={data}>
+          <XAxis
+            dataKey="name"
+            stroke="#888888"
+            fontSize={12}
+            tickLine={false}
+            axisLine={false}
+          />
+          <YAxis
+            stroke="#888888"
+            fontSize={12}
+            tickLine={false}
+            axisLine={false}
+            tickFormatter={(value) => `$${value}`}
+          />
+          <Bar dataKey="total" radius={[4, 4, 0, 0]}>
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={entry.color} />
+            ))}
+          </Bar>
+        </BarChart>
+      </ResponsiveContainer> */}
+      <ResponsiveContainer width="100%" height={300}>
+        <PieChart>
+          <Pie
+            data={data}
+            dataKey="total"
+            name="Total"
+            labelLine={true}
+            label={(name) => `${name.name} - THB${name.total}`}
+            nameKey="name"
+            cx="50%"
+            cy="50%"
+            outerRadius={100}
+          >
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={entry.color} />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+    </>
+  )
+}
