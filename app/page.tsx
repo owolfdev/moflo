5ca46eb342d7c31b9ba8955a8e2a99dc16f00595
owolfdev <owolfdev@gmail.com>
2023-07-12 03:20:53 +0700

    add supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import { cookies } from "next/headers"
 import Link from "next/link"
+import { redirect } from "next/navigation"
 import { settings } from "@/data/settings"
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
 
 import { ExpenseFormAdd } from "@/components/form-add-expense"
 
-export default function IndexPage() {
+export default async function IndexPage() {
+  const supabase = createServerComponentClient({ cookies })
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  if (!user) {
+    redirect("/login")
+  }
   return (
     <section className="container grid items-center gap-6 pt-6 pb-8 md:py-10">
       <div className="flex max-w-[980px] flex-col  gap-2 ">
@@ -13,6 +25,7 @@ export default function IndexPage() {
           </h1>
         </Link>
         <div className="pt-4">
+          <div>Welcome {user.email}</div>
           <ExpenseFormAdd settings={settings} />
         </div>
       </div>
