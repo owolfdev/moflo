81c6f188cab2803d2b32e7e87955aaf1f01771af
owolfdev <owolfdev@gmail.com>
2023-07-12 12:50:16 +0700

    avatar

--- components/navigation/auth-avatar.tsx
+++ components/navigation/auth-avatar.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react"
 import Link from "next/link"
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+import { set } from "date-fns"
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
@@ -12,9 +13,22 @@ interface User {
   // other properties of user...
 }
 
+interface Profile {
+  id: string
+  first_name: string
+  last_name: string
+  avatar: string
+  // other properties of profile...
+}
+
 export function AuthAvatar() {
   const [user, setUser] = useState<User | null>(null)
+  const [profile, setProfile] = useState<Profile | null>(null)
   const supabase = createClientComponentClient()
+
+  useEffect(() => {
+    console.log("profile: ", profile)
+  }, [profile])
 
   useEffect(() => {
     const checkUser = async () => {
@@ -22,16 +36,33 @@ export function AuthAvatar() {
         data: { user },
       } = await supabase.auth.getUser()
       setUser(user)
+
+      console.log("user: ", user)
+
+      if (user) {
+        const { data: profileData, error } = await supabase
+          .from("profiles")
+          .select("*")
+          .eq("id", user.id)
+          .single()
+
+        if (profileData) {
+          setProfile(profileData)
+        } else {
+          console.error("Failed to fetch profile:", error)
+        }
+      }
     }
 
     const { data: authListener } = supabase.auth.onAuthStateChange(
       async (event, session) => {
-        const currentUser = session?.user
-        setUser(currentUser ? currentUser : null)
+        setUser(null)
+        setProfile(null)
+        checkUser()
       }
     )
 
-    checkUser()
+    // checkUser()
 
     // Cleanup function for useEffect
     return () => {
@@ -42,13 +73,14 @@ export function AuthAvatar() {
   }, [supabase.auth])
 
   return (
-    user && (
+    user?.email &&
+    profile?.avatar && (
       <Link href="/profile">
         <div className=" pl-2">
           <Avatar>
             <AvatarImage
-              src="https://avatars.githubusercontent.com/u/108033355"
-              alt="@owolfdev"
+              src={profile?.avatar}
+              alt={`${profile?.first_name} ${profile?.last_name}`}
             />
             <AvatarFallback>OW</AvatarFallback>
           </Avatar>
