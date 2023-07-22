895507e2cb467b0b7d702a26a1f8868664949f3c
owolfdev <owolfdev@gmail.com>
2023-07-23 05:13:33 +0700

    update date on expense page, start MDX

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,24 +1,25 @@
 import fs from "fs"
 import path from "path"
 import React from "react"
-import matter from "gray-matter"
-import { MDXRemote } from "next-mdx-remote"
-import { serialize } from "next-mdx-remote/serialize"
+
+// import matter from "gray-matter"
+// import { MDXRemote } from "next-mdx-remote"
+// import { serialize } from "next-mdx-remote/serialize"
 
 // interface DocumentationProps {
 //   mdxSource: IRenderToString;
 // }
 
 const Documentation = () => {
-  const getMDXSource = async () => {
-    const filePath = path.join(process.cwd(), "content", "documentation.mdx")
-    const fileContents = fs.readFileSync(filePath, "utf8")
-    const { content } = matter(fileContents)
-    const mdxSource = await serialize(content)
-    return mdxSource
-  }
+  // const getMDXSource = async () => {
+  //   const filePath = path.join(process.cwd(), "content", "documentation.mdx")
+  //   const fileContents = fs.readFileSync(filePath, "utf8")
+  //   const { content } = matter(fileContents)
+  //   const mdxSource = await serialize(content)
+  //   return mdxSource
+  // }
 
-  const mdxSource = getMDXSource()
+  // const mdxSource = getMDXSource()
 
   return (
     <section className="container grid items-center gap-6 pt-6 pb-8 md:py-10">
@@ -28,7 +29,7 @@ const Documentation = () => {
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground">
           Moflo is an open-source expense tracking app built with Next.js and
-          Supabase. It's designed to be easy to use and easy to deploy.
+          Supabase. It&apos;s designed to be easy to use and easy to deploy.
         </p>
         {/* <MDXRemote {...mdxSource} /> */}
       </div>
